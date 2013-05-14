62ad85207de1766c1375040d28d660c09227c631
Izumi Wong-Horiuchi <izumiwongh@gmail.com>
2013-05-14 14:59:08 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dailymeal::Application.config.secret_token = 'ddcafaada06cf856b15fd70f356737fb72d3182aab22912e5b4611f85a7cb16f5733907c7e0a30ab2ac104dac6e2555695bffd2380d6eaede6cf1c8ff29c3691'
